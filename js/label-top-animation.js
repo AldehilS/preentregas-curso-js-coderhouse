ff61480bbb32b95f0dfc17b6342df782869edf9c
Aldehil Sánchez <sanheraldehil@outlook.com>
2024-07-08 01:23:03 -0600

    Added login and register page styles. TODO: Fix error message for bad inputs

---
+++ js/label-top-animation.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+
+// Add event listeners to the input fields to move the label to the top
+form.querySelectorAll("div > div").forEach((inputField) => {
+    const label = inputField.querySelector("label");
+    const input = inputField.querySelector("input");
+  
+    // If the input is focused, move the label to the top
+    input.addEventListener("focus", () => {
+      label.classList.add("label-top-position")
+    });
+  
+    // If the input is blurred and there is no value, move the label to the initial position
+    input.addEventListener("blur", () => {
+      if (!input.value) {
+        label.classList.remove("label-top-position")
+      }
+    })
+  })
